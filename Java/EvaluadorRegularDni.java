d7b77a4c9e05921fbfa29338d441786323de2206
crisjsg <dawcrisjsg@gmail.com>
2016-01-29 13:49:11 +0100

    Actualizacion de Expresion Regular

--- Java/EvaluadorRegularDni.java
+++ Java/EvaluadorRegularDni.java
@@ -8,21 +8,28 @@ public class EvaluadorRegularDni {
 	public static void main(String[] args) {
 	
 		//Evaluadores regulares
-			//Numeros permitidos, solo del 0 a 9. Y que sean exactamente 8 numeros.
-		String numerosDniValido = "[0-9]{8}";
-		
-			//Letras no permitidas: I, Ñ, O, U y de la "a" a la "z" (minusculas). Y que haya solo una letra.
-		String letrasDniValida = "[^a-zIÑOU]{1}";
+			/*Numeros permitidos, solo del 0 a 9. Y que sean exactamente 8 numeros y 
+			 * letras no permitidas: I, Ñ, O, U y de la "a" a la "z" (minusculas). Y que haya solo una letra.
+			 * Añadido expresiones para NIE: Que comience con X, Y o Z, que hayan 7 numeros entre 0 y 9 y
+			 * que acabe con una letra de las permitidas.
+			 */
+		String dniNieValido = "[0-9]{8}[^a-zIÑOU]{1}|[XYZ]{1}[0-9]{7}[^a-zIÑOU]{1}";
+		//String numerosDniValido = "[0-9]{8}";
+		//String letrasDniValida = "[^a-zIÑOU]{1}";
 		
 		//Casos test
 		
 		String pruebaDni = "43181554C";
-		Pattern patronDni = Pattern.compile(numerosDniValido + letrasDniValida);
-		Matcher checkPatronDni  = patronDni.matcher(pruebaDni);
+		Pattern patronDniNie = Pattern.compile(dniNieValido);
+		Matcher checkPatronDniNie  = patronDniNie.matcher(pruebaDni);
 		
-		if (checkPatronDni.find()){
-			String dni = checkPatronDni.group();
-			System.out.println("El DNI es: " + dni);
+		if (checkPatronDniNie.find()){
+			String dniOrNie = checkPatronDniNie.group();
+			System.out.println("El DNI o NIE: " + dniOrNie + " es correcto.");
+		}
+		else{
+			String dniOrNie = checkPatronDniNie.group();
+			System.out.println("El DNI o NIE: " + dniOrNie + " es incorrecto.");
 		}
 	
 		
